9ff7b49b775e14af85a5997a622f20a6e7af54b9
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-01 22:51:40 -0500

    moved all require/ require_relatives to config/environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative '../lib/alotacookies/version.rb'
 require_relative '../lib/cli.rb'
 require_relative '../lib/scraper.rb'
 require_relative '../lib/alotacookies.rb'
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
